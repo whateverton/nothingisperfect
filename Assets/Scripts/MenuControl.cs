ad6fa670d666b08c91ff66b15e3bd63f2aa75572
Whateverton <everton.vilela@live.com>
2017-06-22 23:23:47 -0300

    - Adicionada animação no caderno - Menu inicial quase completo - Adicionada font

---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+    Animator animator;
+
+    void OnEnable()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("Opened", true);
+
+        for(int i = 0; i < transform.childCount; ++i)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        StartCoroutine("ActivateButtons");
+    }
+
+    public void LoadScene(string name)
+    {
+        animator.SetBool("Opened", false);
+
+        StartCoroutine("LoadSceneCoroutine", name);
+    }
+
+    public void QuitGame()
+    {
+        animator.SetBool("Opened", false);
+        StartCoroutine("QuitGameCoroutine");
+    }
+
+    IEnumerator LoadSceneCoroutine(string name)
+    {
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene(name);
+
+    }
+
+    IEnumerator QuitGameCoroutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        Application.Quit();
+    }
+
+    IEnumerator ActivateButtons()
+    {
+        yield return new WaitForSeconds(2f);
+
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
